d8130cc17906c7409c99cdfc4c0982083b0ce1f8
wfnuser <wfnuser@126.com>
2019-09-04 20:33:01 +0800

    feat: support antd styles in storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,7 +1,16 @@
 module.exports = async ({ config }) => {
   config.module.rules.push({
     test: /\.less$/,
-    use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+    use: [
+      { loader: "style-loader" },
+      { loader: "css-loader" },
+      {
+        loader: "less-loader",
+        options: {
+          javascriptEnabled: true,
+        },
+      },
+    ],
   });
 
   return config;
